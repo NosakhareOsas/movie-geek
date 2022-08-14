2d1eb11b5354aa6daf437017d65b08aee074e115
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-08-14 23:01:02 +0100

    add search icon

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,21 @@
-
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 function Search ({onSearch}){
     function handleClick(e){
         onSearch(e.target.value)
     }
     return(
-        <div>  
-            <input type='text' placeholder='Search...' onChange={handleClick}/>  
-        </div>
+ 
+        <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">&#x1F50E;</InputGroup.Text>
+            <Form.Control
+            placeholder="Search for your favorite movie..."
+            aria-label="Search"
+            aria-describedby="basic-addon1"
+            onChange={handleClick}
+            />
+      </InputGroup>
+
     );
 }
 
